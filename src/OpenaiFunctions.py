427ecbd33f192cd60d82ced2199324cff5f3ba2b
Christanfirecup <christianfirecup@aol.com>
2023-12-25 11:27:20 -0500

    finished implementing basic thread logic

---
+++ src/OpenaiFunctions.py
@@ -0,0 +1,59 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+load_dotenv()
+client = OpenAI(api_key=os.getenv('OPENAI'))
+
+
+def CreateThread():
+    return client.beta.threads.create()
+
+
+def NewMessage(usermessage, threadid):
+    return client.beta.threads.messages.create(
+        thread_id=threadid,
+        role="user",
+        content=usermessage
+
+    )
+
+
+def CreateRun(threadID, AssistantID):
+    return client.beta.threads.runs.create(
+        thread_id=threadID,
+        assistant_id=AssistantID,
+
+    )
+
+
+def result(threadID, runId, ):
+
+    received = False
+    while not received:
+
+        # Retrieve the latest run status
+        results = client.beta.threads.runs.retrieve(
+            thread_id=threadID,
+            run_id=runId
+        )
+
+        # Check if the run is completed
+        if results.status == 'completed':
+
+            messages = client.beta.threads.messages.list(thread_id=threadID)
+            for message in messages.data:
+                role = message.role
+                content = message.content[0].text.value
+                received = True
+                return f"{role.capitalize()}: {content}"
+
+
+
+
+
+if __name__ == "__main__":
+    newthread = CreateThread()
+    NewMessage("What is your objective", newthread.id)
+    newrun = CreateRun(newthread.id, "asst_Mv3MzfjZUw4pIUT8jvgWwbMP")
+    print(result(newthread.id, newrun.id))
+
---
+++ src/main.py
@@ -0,0 +1,55 @@
+import asyncio
+
+from dotenv import load_dotenv
+import os
+import OpenaiFunctions as AIAPI
+
+load_dotenv()
+
+import discord
+
+user_threads = {}
+intents = discord.Intents.default()
+intents.message_content = True
+Disclient = discord.Client(intents=intents)
+
+
+@Disclient.event
+async def on_ready():
+    print(f'We have logged in as {Disclient.user}')
+
+
+@Disclient.event
+async def on_message(message):
+    if message.author == Disclient.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('$ask'):
+        user_id = message.author.id
+        print(user_id)
+
+        if user_id not in user_threads:
+            newthread = AIAPI.CreateThread()
+            user_threads[user_id] = newthread.id
+        try:
+            thread_id = user_threads[user_id]
+            AIAPI.NewMessage(message.content[5:], thread_id)  # Skip the "$ask " part
+            newrun = AIAPI.CreateRun(thread_id, "asst_4vVvLRtPznGhushUvMcPSqiG")
+            response = AIAPI.result(thread_id, newrun.id)
+
+            # Split the response into 2000-character chunks
+            chunks = [response[i:i + 2000] for i in range(0, len(response), 2000)]
+
+            # Send each chunk in a separate message
+            for chunk in chunks:
+                await message.channel.send(chunk)
+                await asyncio.sleep(1)  # Optional: slight delay between messages
+
+        except Exception as e:
+            await message.channel.send(f"An error occurred: {e}")
+
+
+Disclient.run(os.getenv('TOKEN'))
